b6cc6ba861d8140a6cb0f8696d4b902a07d4d132
Torikul Islam <98918974+SajolX@users.noreply.github.com>
2026-06-21 00:27:03 +0600

    Remove showMsg and toggleMenu functions, add scrollToContact

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function showMsg(){
-document.getElementById("msg").innerText =
-"Hello Babuu 😄 Your website is working perfectly!";
-}
-
-function toggleMenu(){
-let menu = document.getElementById("menu");
-
-if(menu.style.display === "flex"){
-menu.style.display = "none";
-}else{
-menu.style.display = "flex";
-}
+function scrollToContact(){
+document.getElementById("contact").scrollIntoView({behavior:"smooth"});
 }
